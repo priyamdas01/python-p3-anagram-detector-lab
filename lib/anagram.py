bdac097d69e15211db07ad6b22e00937f6f5a101
Priyam <12priyamdas34@gmail.com>
2023-07-07 03:01:51 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        l = []
+        for each_anagram in anagram_list:
+            ll = sorted(list(each_anagram))
+            llword = sorted(list(self.word))
+            if (llword == ll):
+                l.append(each_anagram)
+            
+        return l
+            
+
